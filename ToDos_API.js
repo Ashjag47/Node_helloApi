69c2babe0acef152d2af1a8481e457013e717da0
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-24 20:31:33 +0530

    Feat: Add endpoint to Create a task in ToDo list

--- ToDos_API.js
+++ ToDos_API.js
@@ -29,6 +29,28 @@ const server = http.createServer((req, res) => {
     res.end(s)
   }
 
+  // To Create a task in to do list
+  else if(req.url=="/tasks" && req.method==="POST"){
+    console.log("Executing the code")
+    let data = ""
+    req.on("data", chunk => {
+        data+= chunk.toString()
+    })
+    req.on("end", ()=> {
+        todos.push(JSON.parse(data))
+        res.writeHead(200,{'Content-Type': 'application/json'})
+        n=String(todos.length-1)
+        todos[n]["id"]=n
+        todos[n]["isCompleted"]=false
+        console.log(todos)
+        s=""
+        todos.forEach(todo => {
+          s+=(todo['name']+"|| Is completed: "+todo["isCompleted"]+"\n");
+        });
+        res.end(s)
+    })
+  }
+
 });
 
 server.listen(port, hostname, () => {
